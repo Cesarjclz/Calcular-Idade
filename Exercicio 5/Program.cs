f6448ac08f8656691c63e4c8bd0373d92a66d7b4
César Aguiar <Cesarphz21@gmail.com>
2021-10-20 12:21:27 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio 5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercicio_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+          
+            int nascimento, atual, proximo, a;
+           
+           
+            Console.WriteLine(" digite sua data de nascimento: ");
+            nascimento = int.Parse(Console.ReadLine());
+            Console.WriteLine(" qual o ano atual: ");
+            atual = int.Parse(Console.ReadLine());
+            a = atual - nascimento;
+            proximo = 2022 - nascimento;
+            Console.WriteLine("A sua idade é " + a + " Em 2022 você terá " + proximo);
+  
+
+
+
+
+        }
+    }
+}
